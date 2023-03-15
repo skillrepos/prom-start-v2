d5ee33825758830cc1b3fdaf38097b6347000fe1
Brent Laster <bclaster@nclasters.org>
2023-03-15 19:46:57 -0400

    Update start-mini2.sh

--- extra/start-mini2.sh
+++ extra/start-mini2.sh
@@ -1,5 +1,10 @@
 sudo minikube stop
-sudo minikube start --vm-driver=none --addons=dashboard --kubernetes-version=v1.21.1
+sudo minikube delete
+sudo curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.28.0/minikube-linux-amd64 && sudo chmod +x minikube && sudo cp minikube /usr/local/bin/ && sudo rm minikube
+sudo minikube start --vm-driver=none --kubernetes-version=1.23.1
+sudo mv /home/diyuser3/.kube /home/diyuser3/.minikube $HOME
+sudo chown -R $USER $HOME/.kube $HOME/.minikube
+sudo chown -R $USER $HOME/.minikube; chmod -R u+wrx $HOME/.minikube
 until [ -d /tmp/hostpath-provisioner/monitoring ]
 do 
   sleep 5
