23e3eafc90e9d8c60ebaa2cc125e8d47447f724d
Brent Laster <bclaster@nclasters.org>
2023-03-16 20:37:04 -0400

    Create install-apps.sh

---
+++ extra/install-apps.sh
@@ -0,0 +1,12 @@
+kubectl create ns monitoring
+wget https://get.helm.sh/helm-v3.9.2-linux-amd64.tar.gz
+tar xvf helm-v3.9.2-linux-amd64.tar.gz
+sudo mv linux-amd64/helm /usr/local/bin
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+helm install prom-start prometheus-community/prometheus -n monitoring --set server.service.nodePort=31000 --set server.service.type=NodePort
+~/prom-start/extra/fixtmp2.sh
+helm install grafana grafana/grafana --set service.type=NodePort --set service.nodePort=31750
+kubectl -n monitoring patch svc prom-start-alertmanager --type='json' -p '[{"op":"replace","path":"/spec/type","value":"NodePort"},{"op":"replace","path":"/spec/ports/0/nodePort","value":31500}]'
+echo "!!! Grafana password follows !!!"
+kubectl get secret --namespace monitoring grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
