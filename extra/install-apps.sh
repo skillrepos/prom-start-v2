690a3ffc6239d7b64fbfc99d83f8d40d32e56647
Brent Laster <bclaster@nclasters.org>
2023-03-16 23:00:09 -0400

    Update install-apps.sh

--- extra/install-apps.sh
+++ extra/install-apps.sh
@@ -6,6 +6,6 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo update
 helm install prom-start prometheus-community/prometheus -n monitoring --set server.service.nodePort=31000 --set server.service.type=NodePort
 ~/prom-start/extra/fixtmp2.sh
-helm install grafana grafana/grafana --set service.type=NodePort --set service.nodePort=31750
+helm install grafana grafana/grafana -n monitoring --set service.type=NodePort --set service.nodePort=31750
 kubectl -n monitoring patch svc prom-start-alertmanager --type='json' -p '[{"op":"replace","path":"/spec/type","value":"NodePort"},{"op":"replace","path":"/spec/ports/0/nodePort","value":31500}]'
 
