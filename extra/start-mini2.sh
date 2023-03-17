953245a3076f999bc20166829208e76beb7be61b
Brent Laster <bclaster@nclasters.org>
2023-03-16 20:36:34 -0400

    Update start-mini2.sh

--- extra/start-mini2.sh
+++ extra/start-mini2.sh
@@ -5,12 +5,5 @@ sudo minikube start --vm-driver=none --kubernetes-version=1.23.1
 sudo mv /home/diyuser3/.kube /home/diyuser3/.minikube $HOME
 sudo chown -R $USER $HOME/.kube $HOME/.minikube
 sudo chown -R $USER $HOME/.minikube; chmod -R u+wrx $HOME/.minikube
-kubectl create ns monitoring
-wget https://get.helm.sh/helm-v3.9.2-linux-amd64.tar.gz
-tar xvf helm-v3.9.2-linux-amd64.tar.gz
-sudo mv linux-amd64/helm /usr/local/bin
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update
-helm install prom-start prometheus-community/prometheus -n monitoring --set server.service.nodePort=31000 --set server.service.type=NodePort
-~/prom-start/extra/fixtmp2.sh
+./install-apps.sh
 
