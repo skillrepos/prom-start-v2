5668d97e5f743caa60372407119518194a469c2f
techupskills <82771267+techupskills@users.noreply.github.com>
2024-02-22 07:55:05 -0500

    Update install-apps.sh

--- extra/install-apps.sh
+++ extra/install-apps.sh
@@ -1,3 +1,5 @@
+echo ...Removing any old minikube instances
+minikube delete
 echo ...Starting minikube
 minikube start
 echo ...Creating namespace
@@ -8,9 +10,10 @@ helm repo update
 echo ...Installing Prometheus and Grafana
 helm install -n monitoring monitoring --version="38.0.3" prometheus-community/kube-prometheus-stack --set prometheusOperator.admissionWebhooks.failurePolicy=Ignore
 echo ...Waiting on resources to be ready
-kubectl -n monitoring wait pod --all --for=condition=Ready
-# echo ...Forwarding ports
-# kubectl port-forward -n monitoring svc/monitoring-kube-prometheus-prometheus  :9090 &
-# kubectl port-forward -n monitoring svc/monitoring-prometheus-node-exporter :9100 &
-# kubectl port-forward -n monitoring svc/monitoring-kube-prometheus-alertmanager  :9093 &
-# kubectl port-forward -n monitorning svc/monitoring-grafana :80 &
+kubectl -n monitoring wait --timeout 180s pod --all --for=condition=Ready
+echo ...Forwarding ports
+nohup kubectl port-forward -n monitoring svc/monitoring-kube-prometheus-prometheus  35200:9090 &
+nohup kubectl port-forward -n monitoring svc/monitoring-prometheus-node-exporter 35300:9100 &
+nohup kubectl port-forward -n monitoring svc/monitoring-kube-prometheus-alertmanager  35400:9093 &
+nohup kubectl port-forward -n monitoring svc/monitoring-grafana 35500:80 &
+
