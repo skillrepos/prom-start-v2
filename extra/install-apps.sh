a57108e7a256168d0172d5178e9ab1c937c1af15
techupskills <82771267+techupskills@users.noreply.github.com>
2024-02-22 06:06:01 -0500

    Update install-apps.sh

--- extra/install-apps.sh
+++ extra/install-apps.sh
@@ -5,16 +5,12 @@ kubectl create ns monitoring
 echo ...Adding prometheus-community repo
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
-echo ...Installing Prometheus
-helm install prom-start prometheus-community/prometheus -n monitoring --set server.service.nodePort=31000 --set server.service.type=NodePort
-~/prom-start/extra/fixtmp2.sh
-echo ...Installing Grafana
-helm install grafana grafana/grafana -n monitoring --set service.type=NodePort --set service.nodePort=31750
-echo ...Updating
-kubectl -n monitoring patch svc prom-start-alertmanager --type='json' -p '[{"op":"replace","path":"/spec/type","value":"NodePort"},{"op":"replace","path":"/spec/ports/0/nodePort","value":31500}]'
-while [[ $(kubectl get pods prom-start-alertmanager-0 -n monitoring -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for pod" && sleep 1; done
-kubectl label pod/prom-start-alertmanager-0 app=prometheus -n monitoring
-kubectl label pod/prom-start-alertmanager-0 component=alertmanager -n monitoring
+echo ...Installing Prometheus and Grafana
+helm install -n monitoring monitoring --version="38.0.3" prometheus-community/kube-prometheus-stack --set prometheusOperator.admissionWebhooks.failurePolicy=Ignore
+echo ...Waiting on resources to be ready
+kubectl -n monitoring wait pod --all --for=condition=Ready
 echo ...Forwarding ports
-kubectl port-forward -n monitoring svc/prom-start-prometheus-server :80 &
-kubectl port-forward -n monitoring svc/prom-start-prometheus-node-exporter :9100 &
+kubectl port-forward -n monitoring svc/monitoring-kube-prometheus-prometheus  :9090 &
+kubectl port-forward -n monitoring svc/monitoring-prometheus-node-exporter :9100 &
+kubectl port-forward -n monitoring svc/monitoring-kube-prometheus-alertmanager  :9093 &
+kubectl port-forward -n monitorning svc/monitoring-grafana :80 &
