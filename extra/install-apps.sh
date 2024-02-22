110b87bdd68feb61e4e11505e1876fa71e342581
techupskills <82771267+techupskills@users.noreply.github.com>
2024-02-22 06:27:32 -0500

    Update install-apps.sh

--- extra/install-apps.sh
+++ extra/install-apps.sh
@@ -9,8 +9,8 @@ echo ...Installing Prometheus and Grafana
 helm install -n monitoring monitoring --version="38.0.3" prometheus-community/kube-prometheus-stack --set prometheusOperator.admissionWebhooks.failurePolicy=Ignore
 echo ...Waiting on resources to be ready
 kubectl -n monitoring wait pod --all --for=condition=Ready
-echo ...Forwarding ports
-kubectl port-forward -n monitoring svc/monitoring-kube-prometheus-prometheus  :9090 &
-kubectl port-forward -n monitoring svc/monitoring-prometheus-node-exporter :9100 &
-kubectl port-forward -n monitoring svc/monitoring-kube-prometheus-alertmanager  :9093 &
-kubectl port-forward -n monitorning svc/monitoring-grafana :80 &
+# echo ...Forwarding ports
+# kubectl port-forward -n monitoring svc/monitoring-kube-prometheus-prometheus  :9090 &
+# kubectl port-forward -n monitoring svc/monitoring-prometheus-node-exporter :9100 &
+# kubectl port-forward -n monitoring svc/monitoring-kube-prometheus-alertmanager  :9093 &
+# kubectl port-forward -n monitorning svc/monitoring-grafana :80 &
