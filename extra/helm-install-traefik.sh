78148f48fe875eb60584a2b18c534594cf49e640
gwstudent2 <bl1@nclasters.org>
2024-02-25 07:50:03 -0500

    Convert to use codespace (#1)

--- extra/helm-install-traefik.sh
+++ extra/helm-install-traefik.sh
@@ -1,3 +1,5 @@
 helm repo update
-helm install traefik traefik/traefik -n traefik -f ~/prom-start/extra/traefik-values.yaml
+helm install traefik traefik/traefik -n traefik -f /workspaces/prom-start-v2/extra/traefik-values.yaml
+while [[ $(kubectl get pods -n traefik -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for pod" && sleep 1; done
+nohup kubectl port-forward -n traefik svc/traefik 35600:9100 >&/dev/null &
 
--- extra/install-apps.sh
+++ extra/install-apps.sh
@@ -4,16 +4,19 @@ echo ...Starting minikube
 minikube start
 echo ...Creating namespace
 kubectl create ns monitoring
-echo ...Adding prometheus-community repo
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
-echo ...Installing Prometheus and Grafana
-helm install -n monitoring monitoring --version="38.0.3" prometheus-community/kube-prometheus-stack --set prometheusOperator.admissionWebhooks.failurePolicy=Ignore
+helm install prom-start prometheus-community/prometheus -n monitoring
+helm install grafana grafana/grafana -n monitoring
 echo ...Waiting on resources to be ready
+while [[ $(kubectl get pods prom-start-alertmanager-0 -n monitoring -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for pod" && sleep 1; done
 kubectl -n monitoring wait --timeout 180s pod --all --for=condition=Ready
+kubectl label pod/prom-start-alertmanager-0 app=prometheus -n monitoring
+kubectl label pod/prom-start-alertmanager-0 component=alertmanager -n monitoring
 echo ...Forwarding ports
-nohup kubectl port-forward -n monitoring svc/monitoring-kube-prometheus-prometheus  35200:9090 &
-nohup kubectl port-forward -n monitoring svc/monitoring-prometheus-node-exporter 35300:9100 &
-nohup kubectl port-forward -n monitoring svc/monitoring-kube-prometheus-alertmanager  35400:9093 &
-nohup kubectl port-forward -n monitoring svc/monitoring-grafana 35500:80 &
+nohup kubectl port-forward -n monitoring svc/prom-start-prometheus-server  35200:80 >&/dev/null &
+nohup kubectl port-forward -n monitoring svc/prom-start-prometheus-node-exporter 35300:9100 >&/dev/null &
+nohup kubectl port-forward -n monitoring svc/prom-start-alertmanager  35400:9093 >&/dev/null &
+nohup kubectl port-forward -n monitoring svc/grafana 35500:80 >&/dev/null &
 
