584b23ed3d6b7e8ecc2af88b72ec68f6bed56dd1
techupskills <82771267+techupskills@users.noreply.github.com>
2024-02-28 12:00:24 -0500

    Update helm-install-traefik.sh

--- extra/helm-install-traefik.sh
+++ extra/helm-install-traefik.sh
@@ -1,3 +1,4 @@
+helm repo add traefik https://traefik.github.io/charts
 helm repo update
 helm install traefik traefik/traefik -n traefik -f /workspaces/prom-start-v2/extra/traefik-values.yaml
 while [[ $(kubectl get pods -n traefik -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for pod" && sleep 1; done
