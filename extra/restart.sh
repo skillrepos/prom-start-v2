8c85eced173d9df8b20d27bd8bf805d8bd9249d9
Brent Laster <bclaster@nclasters.org>
2025-02-13 11:11:10 -0500

    Update restart.sh

--- extra/restart.sh
+++ extra/restart.sh
@@ -3,7 +3,9 @@ minikube start
 while [[ $(kubectl get pods prom-start-alertmanager-0 -n monitoring -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for pod" && sleep 1; done
 kubectl -n monitoring wait --timeout 180s pod --all --for=condition=Ready
 echo ...Forwarding ports
+nohup kubectl port-forward -n roar svc/roar-web 31790:8089 >&/dev/null &
 nohup kubectl port-forward -n monitoring svc/prom-start-prometheus-server  35200:80 >&/dev/null &
 nohup kubectl port-forward -n monitoring svc/prom-start-prometheus-node-exporter 35300:9100 >&/dev/null &
 nohup kubectl port-forward -n monitoring svc/prom-start-alertmanager  35400:9093 >&/dev/null &
 nohup kubectl port-forward -n monitoring svc/grafana 35500:80 >&/dev/null &
+/workspaces/prom-start-v2/mysql-ex/update-db.sh roar
